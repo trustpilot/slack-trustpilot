2bdc83398051c872bdd4bb79df146f5dacb0b31b
martinbuberl <mbl@trustpilot.com>
2017-03-23 17:11:36 +0100

    Fixed no-use-before-def errors

--- src/slackapp.js
+++ src/slackapp.js
@@ -6,15 +6,6 @@ const _S = require('underscore.string');
 const moment = require('moment');
 const bluebird = require('bluebird');
 
-module.exports = function (config, businessUnitProvider, trustpilot) {
-  var slackapp = botkit.slackbot({
-    debug: false,
-    retry: 2
-  });
-  setupApp(slackapp, config, businessUnitProvider, trustpilot);
-  return slackapp;
-};
-
 function setupApp(slackapp, config, businessUnitProvider, trustpilot) {
 
   /*
@@ -56,71 +47,6 @@ function setupApp(slackapp, config, businessUnitProvider, trustpilot) {
       });
     });
   });
-
-  /*
-    Entry points
-  */
-
-  slackapp.hears(['.*'], ['direct_message'], (bot, message) => {
-    bot.reply(message, {
-      text: `I need to be invited to a channel in order to work (my permissions on Slack are a bit silly that way).
- Use one of your existing channels or create a new one, it's up to you!`
-    });
-  });
-
-  slackapp.hears(['[1-5] stars?', 'la(te)?st'], ['direct_mention'], (bot, message) => {
-    if (message.thread_ts) {
-      bot.reply(message, {
-        text: 'Looks like we\'re in a thread? I\'m confused! I can handle your request if you ask me in a channel.'
-      });
-      return;
-    }
-    var nbStars = Number(message.text.split(' ')[0]);
-    nbStars = isNaN(nbStars) ? null : nbStars;
-    var slackTeamId = bot.team_info.id;
-
-    businessUnitProvider.getTeamBusinessUnitId(slackTeamId).then(function (businessUnitId) {
-      trustpilot.getLastUnansweredReview(nbStars, businessUnitId).then(function (lastReview) {
-        if (lastReview) {
-          bot.reply(message, formatReview(lastReview));
-        }
-      });
-    });
-  });
-
-  slackapp.on('interactive_message_callback', (bot, message) => {
-    if (message.token !== config.VERIFICATION_TOKEN) {
-      return;
-    }
-    switch (message.actions[0].value) {
-      case 'step_1_write_reply':
-        askForReply(bot, message);
-        break;
-      case 'step_2_send_reply':
-        handleReply(bot, message);
-        break;
-    }
-  });
-
-  /*
-    Incoming webhook plumbing
-  */
-
-  slackapp.findBot = function (teamId) {
-    return _.find(_bots, (bot) => {
-      return bot.team_info.id === teamId;
-    });
-  };
-
-  slackapp.postNewReview = function (review, teamId) {
-    var bot = slackapp.findBot(teamId);
-    if (bot) {
-      var message = formatReview(review);
-      message.username = bot.config.bot.name; // Confusing, but such is life
-      message.channel = bot.config.incoming_webhook.channel;
-      bot.send(message);
-    }
-  };
 
   /*
     Internal workings
@@ -192,6 +118,28 @@ function setupApp(slackapp, config, businessUnitProvider, trustpilot) {
     });
   }
 
+  function collectUserMessages(bot, user, channel, threadTs) {
+    bluebird.promisifyAll(bot.api.channels);
+
+    return bot.api.channels.repliesAsync({
+      'token': bot.config.incoming_webhook.token,
+      'channel': channel,
+      'thread_ts': threadTs
+    }).then((data) => {
+      if (data && data.hasOwnProperty('messages')) {
+        var fullText = data.messages.filter((message) => {
+          return message.user === user;
+        })
+        .map((message) => {
+          return message.text;
+        })
+        .join('\n');
+        return fullText;
+      }
+      return null;
+    });
+  }
+
   function handleReply(bot, message) {
     var currentChannel = message.channel;
     var ts = message.original_message.ts;
@@ -219,25 +167,77 @@ function setupApp(slackapp, config, businessUnitProvider, trustpilot) {
     });
   }
 
-  function collectUserMessages(bot, user, channel, threadTs) {
-    bluebird.promisifyAll(bot.api.channels);
+  /*
+    Entry points
+  */
 
-    return bot.api.channels.repliesAsync({
-      'token': bot.config.incoming_webhook.token,
-      'channel': channel,
-      'thread_ts': threadTs
-    }).then((data) => {
-      if (data && data.hasOwnProperty('messages')) {
-        var fullText = data.messages.filter((message) => {
-          return message.user === user;
-        })
-        .map((message) => {
-          return message.text;
-        })
-        .join('\n');
-        return fullText;
-      }
-      return null;
+  slackapp.hears(['.*'], ['direct_message'], (bot, message) => {
+    bot.reply(message, {
+      text: `I need to be invited to a channel in order to work (my permissions on Slack are a bit silly that way).
+ Use one of your existing channels or create a new one, it's up to you!`
     });
-  }
+  });
+
+  slackapp.hears(['[1-5] stars?', 'la(te)?st'], ['direct_mention'], (bot, message) => {
+    if (message.thread_ts) {
+      bot.reply(message, {
+        text: 'Looks like we\'re in a thread? I\'m confused! I can handle your request if you ask me in a channel.'
+      });
+      return;
+    }
+    var nbStars = Number(message.text.split(' ')[0]);
+    nbStars = isNaN(nbStars) ? null : nbStars;
+    var slackTeamId = bot.team_info.id;
+
+    businessUnitProvider.getTeamBusinessUnitId(slackTeamId).then(function (businessUnitId) {
+      trustpilot.getLastUnansweredReview(nbStars, businessUnitId).then(function (lastReview) {
+        if (lastReview) {
+          bot.reply(message, formatReview(lastReview));
+        }
+      });
+    });
+  });
+
+  slackapp.on('interactive_message_callback', (bot, message) => {
+    if (message.token !== config.VERIFICATION_TOKEN) {
+      return;
+    }
+    switch (message.actions[0].value) {
+      case 'step_1_write_reply':
+        askForReply(bot, message);
+        break;
+      case 'step_2_send_reply':
+        handleReply(bot, message);
+        break;
+    }
+  });
+
+  /*
+    Incoming webhook plumbing
+  */
+
+  slackapp.findBot = function (teamId) {
+    return _.find(_bots, (bot) => {
+      return bot.team_info.id === teamId;
+    });
+  };
+
+  slackapp.postNewReview = function (review, teamId) {
+    var bot = slackapp.findBot(teamId);
+    if (bot) {
+      var message = formatReview(review);
+      message.username = bot.config.bot.name; // Confusing, but such is life
+      message.channel = bot.config.incoming_webhook.channel;
+      bot.send(message);
+    }
+  };
 }
+
+module.exports = function (config, businessUnitProvider, trustpilot) {
+  var slackapp = botkit.slackbot({
+    debug: false,
+    retry: 2
+  });
+  setupApp(slackapp, config, businessUnitProvider, trustpilot);
+  return slackapp;
+};
