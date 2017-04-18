454c791b74a18481afe7cc5ecb5e48562e8fbfc0
Miklos Aubert <mat@trustpilot.com>
2017-04-18 17:35:03 +0200

    Stop using RTM API, ensure we ACK slash commands

--- src/oauth-handler.js
+++ src/oauth-handler.js
@@ -2,6 +2,7 @@ module.exports = function (err, req, res) {
   if (err) {
     res.status(500).send(`ERROR: ${err}`);
   } else {
+    console.info(`Use /incoming-webhooks/${req.identity.team_id} to receive new reviews.`);
     res.redirect(req.identity.url);
   }
 };
