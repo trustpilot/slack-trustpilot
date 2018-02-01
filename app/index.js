c8b36f6cdf1f657eb341065e8cce6acb0e2b4ab1
Miklos Aubert <mat@trustpilot.com>
2018-02-01 16:13:20 +0100

    Make app runnable as a module

--- app/index.js
+++ app/index.js
@@ -2,28 +2,49 @@
     Entry point of this app, sets us up and running from config and env variables
 */
 
-const config = require('./config.js');
+const TrustpilotApi = require('./trustpilot-api');
+const StorageEngine = require('./storage-engine');
+const SlackApp = require('./slackapp.js');
 
-if (!config.SLACK_CLIENT_ID || !config.SLACK_SECRET) {
-  console.log(`Sorry, you need to give me this app's credentials. Please configure
- SLACK_CLIENT_ID and SLACK_SECRET in config.js`);
+const run = (config, trustpilotClient, webserverExtensions) => {
+  if (!config.SLACK_CLIENT_ID || !config.SLACK_SECRET) {
+    console.log(`Sorry, you need to give me this app's credentials. Please configure
+    SLACK_CLIENT_ID and SLACK_SECRET in config.js`);
 
-  process.exit(-1);
+    process.exit(-1);
+  }
+
+  const trustpilotApi = TrustpilotApi(trustpilotClient);
+  const storageEngine = StorageEngine(config.BOTKIT_STORAGE_TYPE);
+  const slackapp = SlackApp(config, trustpilotApi, storageEngine);
+
+  // Set up a web server to expose oauth and webhook endpoints
+  slackapp.setupWebserver(config.PORT, () => {
+    // Middleware mounting and the like needs to happen before we set up the endpoints
+    const {
+      oAuthCallback,
+    } = webserverExtensions(slackapp, config);
+    slackapp.createWebhookEndpoints(slackapp.webserver, config.VERIFICATION_TOKEN);
+    slackapp.createOauthEndpoints(slackapp.webserver, oAuthCallback);
+  });
+};
+
+if (require.main !== module) {
+  module.exports = run;
+} else {
+  /* eslint-disable global-require */
+  const config = require('./config.js');
+  const webserverExtensions = require('./webserver-extensions');
+  const Trustpilot = require('trustpilot');
+  /* eslint-enable global-require */
+
+  const trustpilotClient = new Trustpilot({
+    apiKey: config.API_KEY,
+    secret: config.API_SECRET,
+    username: config.BUSINESS_USER_NAME,
+    password: config.BUSINESS_USER_PASS,
+    baseUrl: config.API_HOST,
+  });
+
+  run(config, trustpilotClient, webserverExtensions);
 }
-
-const trustpilotApi = require('./trustpilot-api')(config);
-const storageEngine = require('./storage-engine');
-const slackapp = require('./slackapp.js')(config, trustpilotApi, storageEngine);
-const webserverExtensions = require('./webserver-extensions');
-
-const PORT = process.env.PORT || 7142;
-
-// Set up a web server to expose oauth and webhook endpoints
-slackapp.setupWebserver(PORT, () => {
-  // Middleware mounting and the like needs to happen before we set up the endpoints
-  const {
-    oAuthCallback,
-  } = webserverExtensions(slackapp, config, PORT);
-  slackapp.createWebhookEndpoints(slackapp.webserver, config.VERIFICATION_TOKEN);
-  slackapp.createOauthEndpoints(slackapp.webserver, oAuthCallback);
-});
