2790575eb687d62a8970d50d6a5732725346c1b1
Miklos Aubert <mat@trustpilot.com>
2018-11-09 14:47:45 +0100

    Make sure config values are empty if not specified

--- app/config.js
+++ app/config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  // Minimal configuration needed
-  SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID || 'YOUR_SLACK_CLIENT_ID',
-  SLACK_SECRET: process.env.SLACK_SECRET || 'YOUR_SLACK_SECRET',
-  VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN || 'YOUR_VERIFICATION_TOKEN',
-  API_KEY: process.env.API_KEY || 'YOUR_TRUSTPILOT_API_KEY',
-  API_SECRET: process.env.API_SECRET || 'YOUR_TRUSTPILOT_API_SECRET',
-  BUSINESS_USER_NAME: process.env.BUSINESS_USER_NAME || 'YOUR_TRUSTPILOT_BUSINESS_USER_NAME',
-  BUSINESS_USER_PASS: process.env.BUSINESS_USER_PASS || 'YOUR_TRUSTPILOT_BUSINESS_USER_PASS',
-  BUSINESS_UNIT_ID: process.env.BUSINESS_UNIT_ID || 'YOUR_TRUSTPILOT_BUSINESS_UNIT_ID',
+  // Minimal configuration needed.
+  // Use the below environment variables or put values directly between the quotes, e.g.
+  // SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID || '<Your Slack Client ID>',
+  SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID || '',
+  SLACK_SECRET: process.env.SLACK_SECRET || '',
+  VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN || '',
+  API_KEY: process.env.API_KEY || '',
+  API_SECRET: process.env.API_SECRET || '',
+  BUSINESS_USER_NAME: process.env.BUSINESS_USER_NAME || '',
+  BUSINESS_USER_PASS: process.env.BUSINESS_USER_PASS || '',
+  BUSINESS_UNIT_ID: process.env.BUSINESS_UNIT_ID || '',
 
   // Extra configuration (storage etc.)
   BOTKIT_STORAGE_TYPE: process.env.BOTKIT_STORAGE_TYPE || 'file',
