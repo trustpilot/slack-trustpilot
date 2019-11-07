2116cb3c48dacc57e154e762081867c969fe33ea
Miklos Aubert <mat@trustpilot.com>
2019-11-07 13:25:14 +0100

    Rewrote DynamoDB storage from scratch with AWS SDK

--- app/storage-engine/dynamodb.js
+++ app/storage-engine/dynamodb.js
@@ -4,12 +4,45 @@
  To use this, make sure that the following environment variables are set:
  - BOTKIT_STORAGE_TYPE = dynamodb
  - DYNAMO_TABLE = <the name of your table>
- - AWS_DEFAULT_REGION / AWS_ACCESS_KEY_ID / AWS_SECRET_ACCESS_KEY: your usual AWS credentials
+ - AWS_DEFAULT_REGION
 */
-module.exports = require('botkit-storage-dynamodb')({
-  dynamoTable: process.env.DYNAMO_TABLE,
-  region: process.env.AWS_DEFAULT_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  sessionToken: process.env.AWS_SESSION_TOKEN,
-});
+const aws = require('aws-sdk');
+const { promisify } = require('util');
+
+const createStorage = (db, table, type) => {
+  return {
+    get: async (id, cb) => {
+      try {
+        const res = await db.getAsync({ TableName: table, Key: { type, id } });
+        return cb(null, res.Item);
+      } catch (e) {
+        // no result found
+        return cb(`No result found for ${id}: ${e.message}`, null);
+      }
+    },
+
+    save: async (data, cb) => {
+      try {
+        const res = await db.putAsync({ TableName: table, Item: { type, ...data } });
+        return cb(null, res);
+      } catch (e) {
+        return cb(e, null);
+      }
+    },
+  };
+};
+
+const getStorage = () => {
+  const db = new aws.DynamoDB.DocumentClient({ region: process.env.AWS_DEFAULT_REGION });
+  db.getAsync = promisify(db.get);
+  db.putAsync = promisify(db.put);
+
+  const storage = {};
+  ['teams', 'channels', 'users'].forEach((type) => {
+    storage[type] = createStorage(db, process.env.DYNAMO_TABLE || 'botkit', type);
+  });
+
+  return storage;
+};
+
+module.exports = getStorage();
